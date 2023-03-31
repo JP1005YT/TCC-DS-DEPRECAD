8c0fec4e4478377e5d868144663dcdd77b1dca27
Michael <michael.vicente1705@gmail.com>
2023-03-31 15:28:55 -0300

    Co-authored-by: JOAO PEDRO GARCIA GIROTTO <joao.girotto2@etec.sp.gov.br>

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+function troca_menu(){
+    let menu = document.querySelector('#menu')
+    let leftside = document.querySelector('.leftside')
+    menu.classList.toggle('ativo')
+    leftside.classList.toggle('ativo')
+}
