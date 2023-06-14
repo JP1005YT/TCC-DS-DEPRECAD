73ad36224ec924c3315474065678374af9e46da1
JP1005YT <godlolpro32@gmail.com>
2023-06-14 18:43:12 -0300

    HASHTAGS 1/2

--- pages/social/main.js
+++ pages/social/main.js
@@ -11,15 +11,50 @@ async function Query_Alguem_Logado(json){
         }
     });
     resposta = await dados.json();
-    console.log(resposta)
     u_infos = resposta
 }
-Query_Alguem_Logado()
 
 function ToNovoPost(){
     document.querySelector("#post-page").classList.toggle("ativo")
 }
 
-function NovoPost(){
-
+function ToNovaTag(){
+    document.querySelector("#tag-page").classList.toggle("ativo")
 }
+
+async function LoadTags() {
+    const dados = await fetch('http://localhost:3333/tags', {
+        method: "POST"
+    });
+    const resposta = await dados.json();
+    console.log(resposta)
+    document.querySelector("#tagsinUsed").innerHTML = `HashTags:${resposta.num}`
+    
+}
+function NovaTag(){
+    let newtag = document.querySelector("#newtag")
+    let obj = {
+        "tag" : newtag.value
+    }
+    QueryNovaTag(obj)
+    newtag.value = ""
+}
+
+async function QueryNovaTag(tag){
+    const dados = await fetch('http://localhost:3333/newtag',{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body:JSON.stringify(tag)
+    });
+    resposta = await dados.json()
+    LoadTags()
+}
+
+function NovoPost(){
+    
+}
+
+LoadTags()
+Query_Alguem_Logado()
